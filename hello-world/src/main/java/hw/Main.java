ac68c71a922251cd7c2f1f31537394f80d20f28a
Paul Merlin <paul@nosphere.org>
2011-01-20 19:56:17 +0100

    Added exec:java config for hello-world

---
+++ hello-world/src/main/java/hw/Main.java
@@ -0,0 +1,63 @@
+package hw;
+
+
+import org.qi4j.api.composite.TransientBuilderFactory;
+import org.qi4j.api.composite.TransientComposite;
+
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.bootstrap.SingletonAssembler;
+
+public class Main
+{
+
+    // ------------------------------------------------------------------------
+    public static interface HelloWorld
+    {
+
+        String sayHello();
+
+    }
+
+    // ------------------------------------------------------------------------
+    public static class HelloWorldImpl
+            implements HelloWorld
+    {
+
+        @Override
+        public String sayHello()
+        {
+            return "Hello Qi4j World !";
+        }
+
+    }
+
+    // ------------------------------------------------------------------------
+    public static interface HelloWorldComposite
+            extends HelloWorld, TransientComposite
+    {
+    }
+
+    // ------------------------------------------------------------------------
+    public static void main( String[] args )
+    {
+        SingletonAssembler assembler = new SingletonAssembler()
+        {
+
+            @Override
+            public void assemble( ModuleAssembly moduleAssembly )
+                    throws AssemblyException
+            {
+                moduleAssembly.addTransients( HelloWorldComposite.class ).withMixins( HelloWorldImpl.class );
+            }
+
+        };
+
+        TransientBuilderFactory factory = assembler.transientBuilderFactory();
+
+        HelloWorld hw = factory.newTransient( HelloWorld.class );
+
+        System.out.println( hw.sayHello() );
+    }
+
+}
